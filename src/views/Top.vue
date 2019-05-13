720f2c9d79734ef9a9d08e8f2372f1f199468982
katasu <katasu9923@gmail.com>
2019-05-13 17:08:15 +0900

    アイテム一覧表示の実装

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,7 +1,16 @@
 <template>
     <div id="top">
-        <div class="container">
-            <div v-for="item in items">{{ item.ownerName }}</div>
+        <div class="wrapper">
+            <div class="container" v-for="item in items">
+                <div class="iconContainer">
+                    <div class="iconBackground"></div>
+                </div>
+                <div class="textContainer">
+                    <div class="textWrapper">名前: {{ item.itemName }}</div>
+                    <div class="textWrapper">アドレス: {{ item.adress }}</div>
+                    <div class="textWrapper">所有者: {{ item.ownerName }}</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -14,17 +23,17 @@ export default {
         items: {
             name1: {
                 itemName: "キノコ",
-                adress: "",
+                adress: "dasmdklamsdklm",
                 ownerName: "マリオ"
             },
             name2: {
                 itemName: "モンスターボール",
-                adress: "",
+                adress: "dmkasldl",
                 ownerName: "サトシ"
             },
             name3: {
                 itemName: "太刀",
-                adress: "",
+                adress: "dl;samdas",
                 ownerName: "ハンター"
             },
         }
@@ -35,15 +44,49 @@ export default {
 
 <style lang="scss" scoped>
 
-#top {
+#top, .wrapper {
   width: 100%;
   height: 100%;
 }
 
-.container {
-    width: 100%;
-    height: 100%;
+.wrapper {
     background-color: #FAFAFA;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
 }
+
+.container {
+    background-color: antiquewhite;
+    width: 80vw;
+    height: 15vh;
+    margin-top: 3em;
+    display: flex;
+}
+
+.iconContainer, .textContainer {
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 12px;
+}
+
+.textContainer {
+    width: 70%;
+    align-items: inherit;
+    flex-direction: column;
+}
+
+.iconBackground {
+    background-color: khaki;
+    width: 20vw;
+    height: 20vw;
+    max-width: 150px;
+    max-height: 150px;
+    border: solid 0 #000000;
+    border-radius: 100px;
+}
+
 </style>
 
