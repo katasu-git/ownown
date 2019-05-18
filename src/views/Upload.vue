1d5855af54af69b89bb73d48b7387f0f96779f8b
ramo <knht31i@gmail.com>
2019-05-19 00:37:13 +0900

    アップロードページのfanction書いた

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -3,6 +3,19 @@
         <div class="wrapper">
             アップロード用のページです！
             <router-link to="/">トップページへ移動</router-link>
+            <!-- <form>
+              <input type="text" placeholder="index" />
+              <input type="text" placeholder="objectname" />
+              <input type="text" placeholder="ownername" />
+              <button type="submit" v-on:click="register">submit</button>
+            </form> -->
+            <button type="submit" v-on:click="register">submit</button>
+
+            <input v-model="index" placeholder="index">
+            <input v-model="objectname" placeholder="objectname">
+            <input v-model="ownername" placeholder="ownername">
+
+            <!-- <p>Message is: {{ message }}</p> -->
         </div>
     </div>
 </template>
@@ -11,7 +24,30 @@
 export default {
     name: 'upload',
   data () {
-    return {}
+    return {
+      // message: this.message,
+      index: this.index,
+      objectname: this.objectname,
+      ownername: this.ownername,
+
+    }
+  },
+  methods: {
+    register: function(){
+      // console.log(this.message);
+
+      console.log(this.index);
+      console.log(this.objectname);
+      console.log(this.ownername);
+      contract.methods.setObject(this.index, this.objectname,this.ownername)
+      .send({ from: coinbase })
+      .on("receipt", function(receipt){
+        console.log("success");
+      })
+      .on("error", function(error){
+         console.log("error"); 
+      });
+    }
   },
 }
 </script>
